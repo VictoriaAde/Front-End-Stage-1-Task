8c664cc8344f3e4c216517d7c82e59ebd7f7015e
Victoria <adedayovicky123@gmail.com>
2022-10-24 08:41:21 +0100

    :lipstick: style header and main
    add styles to links
    
    customise color in tailwind.config

--- src/App.js
+++ src/App.js
@@ -5,20 +5,54 @@ import profileImg from '../static/profile-img.svg';
 const App = () => {
   return (
     <StrictMode>
-      <header className="">
-        <div>
-          <img src={profileImg} alt="" />
-        </div>
-        <h1 className="">Annette Black</h1>
-      </header>
-      <section className="flex flex-col-reverse  bg-red-500">
-        <a href="https://twitter.com/Vickish11">Twitter Link</a>
-        <a href="https://training.zuri.team/">Zuri Team</a>
-        <a href="http://books.zuri.team">Zuri Books</a>
-        <a href="https://books.zuri.team/">Python Books</a>
-        <a href="https://background.zuri.team">Background Check for Coders</a>
-        <a href="https://books.zuri.team/design-rules">Design Books</a>
-      </section>
+      <div className="flex flex-col items-center py-12 bg-milk">
+        <header className="">
+          <div className="mt-5">
+            <img src={profileImg} alt="" />
+          </div>
+          <h1 className="mt-4 mb-14 text-gray-dark font-bold">Annette Black</h1>
+        </header>
+        <main className="flex flex-col gap-6 text-center w-11/12 lg:w-9/12">
+          <a
+            href="https://twitter.com/Vickish11"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Twitter Link
+          </a>
+          <a
+            href="https://training.zuri.team/"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Zuri Team
+          </a>
+          <a
+            href="http://books.zuri.team"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Zuri Books
+          </a>
+          <a
+            href="https://books.zuri.team/"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Python Books
+          </a>
+          <a
+            href="https://background.zuri.team"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Background Check for Coders
+          </a>
+          <a
+            href="https://books.zuri.team/design-rules"
+            className="py-6 px-6 text-gray-dark font-medium bg-gray-light hover:bg-hover-gray rounded-lg"
+          >
+            Design Books
+          </a>
+        </main>
+      </div>
+
+      <footer></footer>
     </StrictMode>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  mode: "jit",
-  content: ["./src/*.{html,js}"],
+  mode: 'jit',
+  content: ['./src/*.{html,js}'],
   theme: {
+    colors: {
+      milk: '#F4F5F7',
+      'gray-dark': '#101828',
+      gray: '#667085',
+      'hover-gray': '#D0D5DD',
+      'gray-light': '#EAECF0',
+    },
     extend: {},
   },
   variants: {},
