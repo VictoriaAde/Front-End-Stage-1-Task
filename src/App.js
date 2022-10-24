b2d8bcb8cbd3945e5c8f4769aa5c12db96616d45
Victoria <adedayovicky123@gmail.com>
2022-10-24 06:34:08 +0100

    :art: add static reporter plugin for static files
    add path to display images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { render } from "react-dom";
-import { StrictMode } from "react";
+import { render } from 'react-dom';
+import { StrictMode } from 'react';
+import profileImg from '../static/profile-img.svg';
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Your name</h1>
-      </div>
+      <header className="">
+        <div>
+          <img src={profileImg} alt="" />
+        </div>
+        <h1 className="">Annette Black</h1>
+      </header>
+      <section className="flex flex-col-reverse  bg-red-500">
+        <a href="https://twitter.com/Vickish11">Twitter Link</a>
+        <a href="https://training.zuri.team/">Zuri Team</a>
+        <a href="http://books.zuri.team">Zuri Books</a>
+        <a href="https://books.zuri.team/">Python Books</a>
+        <a href="https://background.zuri.team">Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules">Design Books</a>
+      </section>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
