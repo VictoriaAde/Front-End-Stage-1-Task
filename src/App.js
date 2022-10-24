b69208e33e5659554f817e5d3ff48e5260f50dd7
Victoria <adedayovicky123@gmail.com>
2022-10-24 16:19:15 +0100

    :lipstick: add styles for share and hamburger icons

--- src/App.js
+++ src/App.js
@@ -5,35 +5,42 @@ import githubIcon from '../static/github.svg';
 import slackIcon from '../static/slack.svg';
 import zuriLogo from '../static/zuri-logo.svg';
 import i4g from '../static/I4G.svg';
+import shareBtn from '../static/share-button.svg';
+import hamburgerBtn from '../static/hamburger-button.svg';
 
 const App = () => {
   return (
     <StrictMode>
       <div className="flex flex-col items-center py-12 bg-milk">
-        <header className="">
+        <div className="absolute top-9 right-9">
+          <img
+            src={hamburgerBtn}
+            alt="Hamburger / menu button"
+            className=" md:hidden "
+          />
+        </div>
+        <div className="absolute top-9 right-44	">
+          <img
+            src={shareBtn}
+            alt="Click on this icon to share this profile"
+            className="hidden md:block "
+          />
+        </div>
+        <header className="relative">
           <div className="mt-5">
             <img
               src={profileImg}
-              alt=""
+              alt="The profile"
               id="profile__img"
               className="mx-auto"
             />
           </div>
           <h1 className="mt-4 mb-12 text-gray-dark font-bold text-lg lg:text-xl">
-            Annette Black
-          </h1>
-          {/* <h2
-            id="twitter"
-            className="mt-4 mb-7 text-gray-dark font-bold text-base lg:text-lg"
-          >
             Vickish11
-          </h2>
-          <h2
-            id="slack"
-            className="mt-4 mb-14 text-gray-dark font-bold text-base lg:text-lg"
-          >
+          </h1>
+          <h2 id="slack" className="hidden">
             vickish
-          </h2> */}
+          </h2>
         </header>
         <main className="flex flex-col gap-6 text-center w-11/12 lg:w-9/12">
           <a
